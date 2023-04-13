80d2286c9f637b5d97f265d878a2d9628ff4054e
Christopher agüero <chris19986@gmail.com>
2023-04-13 17:04:21 -0300

    first-touches

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,19 @@ export default {
   content: ["./index.html", "./src/**/*.{js}"],
   theme: {
     extend: {},
+    colors: {
+      "gr-100": "hsl(171,28,60,50%)",
+      "gr-200": "hsl(171,58,28,65%)",
+      "gr-300": "#1E7164",
+      "gr-400": "#085352",
+    },
+    screens: {
+      sm: "480px",
+      md: "769px",
+      lg: "976px",
+      xl: "1440px",
+    },
+    fontFamily: ["Roboto Slab", "serif"],
   },
   plugins: [],
 };
